906b80d78b6fd26a801a04a33fd36b1b186ea797
João Pedro Limão <jplimao12@gmail.com>
2021-06-11 17:05:33 -0300

    test: Use sklearn knn to check algorithm

--- knn/Main.java
+++ knn/Main.java
@@ -6,6 +6,6 @@ class Main {
     public static void main(String[] args) throws Exception{
         List<Double> list = List.of(6.0, 162.0,62.0,0.0,0.0,24.3,0.178,50.0);
         Knn knn = new Knn(3, list);
-    
+        
     }
 }
--- proof/knn_accuracy.py
+++ proof/knn_accuracy.py
@@ -1,1 +1,23 @@
+#%%
+# Import and read csv
+import pandas as pd
+from sklearn.neighbors import KNeighborsClassifier
 
+path: str = '/home/johnazedo/Documents/UFRN/CPCourse/datasets/diabetes.csv'
+dataframe = pd.read_csv(path)
+
+# %%
+# Separate outcome and predictors
+outcome = dataframe.iloc[:, 0:8]
+predictors = dataframe.iloc[:, 8]
+
+# %%
+# Fit
+knn = KNeighborsClassifier(n_neighbors=3)
+knn.fit(outcome, predictors)
+
+#%%
+# Testing
+print(knn.predict([[6,162,62,0,0,24.3,0.178,50]]))
+
+# %%
