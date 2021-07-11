0f0045b633ea8a899c9a659ef3353338f2fef6d8
João Pedro Limão <jplimao12@gmail.com>
2021-07-11 13:19:19 -0300

    [knn] refactor: Rewrite KNN  class

--- knn/algorithm/Knn.java
+++ knn/algorithm/Knn.java
@@ -8,28 +8,102 @@ import com.opencsv.CSVReader;
 import com.opencsv.CSVReaderBuilder;
 
 class Knn {
-    private static final String CSV_FILE = "/home/johnazedo/Documents/UFRN/CPCourse/datasets/diabetes.csv";
-
     private int k;
-    private List<Double> subject;
     private List<Double[]> data;
+    private List<Double[]> tests;
 
-    public Knn(int k, List<Double> subject) {
+    public Knn(String dataFile, String testFile) {
+        this.data = loadFile(dataFile);
+        this.tests = loadFile(testFile);
+    }
+
+    public void setNumber(int k){
         this.k = k;
-        this.subject = subject;
+    }
 
-        try {
-            data = getData();
+    public Double accuracy(List<Double> outcomes){
+        final int OUTCOME_INDEX = 8;
+        int hits = 0;
+        int index = 0;
 
-        } catch (Exception error) {
-            data = new ArrayList<Double[]>();
-            error.printStackTrace();
+        for(Double[] subject: this.tests){
+            int equal = -1;
+
+            if(index<outcomes.size()){
+                equal = Double.compare(subject[OUTCOME_INDEX], outcomes.get(index));
+            }
+
+            if(equal==0){
+                hits++;
+            }
+
+            index++;
+        }
+
+        return Double.valueOf(hits*100/outcomes.size());
+    }
+
+    public List<Double> predictAll(){
+        List<Double> outcomes = new ArrayList<Double>();
+
+        this.tests.forEach((subject) -> {
+            outcomes.add(predict(subject));
+        });
+
+        return outcomes;
+    }
+
+    public Double predict(Double[] subject){
+        List<Double> results = this.results(subject);
+
+        var amountWithDiabets = Collections.frequency(results, 1.0);
+        var amountWithoutDiabets = Collections.frequency(results, 0.0);
+
+        if(amountWithDiabets>=amountWithoutDiabets){
+           return 1.0;
+        }else{
+            return 0.0;
         }
     }
 
-    // Read file 
-    public List<Double[]> getData() throws Exception {
-        FileReader reader = new FileReader(CSV_FILE);
+    private List<Double> results(Double[] subject) {
+        final Double EUCLIDEAN_PARAM = 2.0;
+        final int OUTCOME_INDEX = 8;
+
+        List<Double> outcomes = new ArrayList<Double>();
+        Double sum, component, distance;
+        Double[] instance;
+
+        for(int i=0; i<this.data.size(); i++){
+            sum = 0.0;
+            instance = this.data.get(i);
+            for(int j=0; j<OUTCOME_INDEX; j++){
+                component = instance[j] - subject[j];
+                sum+=Math.pow(component, EUCLIDEAN_PARAM);
+            }
+            distance = Math.pow(sum, (1/EUCLIDEAN_PARAM));
+
+            if(distance<this.k){
+                outcomes.add(instance[OUTCOME_INDEX]);
+            }
+        }
+        return outcomes;
+    } 
+
+
+    // Read file functions
+    // You can create anothre class with this functions
+    // You can apply single responsibility and dependency inversion principles
+    private List<Double[]> loadFile(String file){
+        try {
+            return getData(file);
+        } catch (Exception error) {
+            return new ArrayList<Double[]>();
+        }
+    }
+
+    private List<Double[]> getData(String file) throws Exception {
+        FileReader reader = new FileReader(file);
         CSVReader csvReader = new CSVReaderBuilder(reader).withSkipLines(1).build();
         List<Double[]> list = new ArrayList<>();
         String[] line;
@@ -43,56 +117,11 @@ class Knn {
         return list;
     }
 
-    public Double[] convert(String[] array){
+    private Double[] convert(String[] array){
         Double[] newArray = new Double[array.length];
         for(int i=0; i<array.length; i++){
             newArray[i] = Double.parseDouble(array[i]);
         }
         return newArray;
     }
-
-    public List<Double> distance() {
-        List<Double> ed = new ArrayList<Double>();
-        int dataSize = this.data.size();
-        Double sum, component;
-        Double[] array;
-        final Double EUCLIDEAN_PARAM = 2.0;
-
-        for(int i=0; i<dataSize; i++){
-            sum = 0.0;
-            array = this.data.get(i);
-            for(int j=0; j<this.subject.size(); j++){
-                component = array[j] - this.subject.get(j);
-                sum+=Math.pow(component, EUCLIDEAN_PARAM);
-            }
-
-            ed.add(Math.pow(sum, (1/EUCLIDEAN_PARAM)));
-        }
-        return ed;
-    }
-
-    public int predict(){
-        List<Double> distances = distance();
-        List<Double> results = new ArrayList<Double>();
-        final int OUTCOME_INDEX = 8;
-
-
-        distances.forEach((distance) -> {
-            if(distance<this.k){
-                results.add(this.data.get(distances.indexOf(distance))[OUTCOME_INDEX]);
-            }
-        });
-
-        
-        var amountWithDiabets = Collections.frequency(results, 1.0);
-        var amountWithoutDiabets = Collections.frequency(results, 0.0);
-
-
-        if(amountWithDiabets>=amountWithoutDiabets){
-           return 1;
-        }else{
-            return 0;
-        }
-    }
- 
 }
